765c90c68f80eda924a9de309da9f8e8baf6597f
Artem Danilov <artem@dnlvs.com>
2023-05-19 01:03:19 +0200

    Fix ssh keys in configuration options

--- lib/kitchen/transport/rsync.rb
+++ lib/kitchen/transport/rsync.rb
@@ -39,15 +39,20 @@ module Kitchen
       # Tracked in https://github.com/test-kitchen/test-kitchen/pull/726
       def create_new_connection(options, &block)
         if @connection
-          logger.debug("[SSH] shutting previous connection #{@connection}")
+          logger.debug("[rsync] shutting previous connection #{@connection}")
           @connection.close
         end
 
         @connection_options = options
-        @connection = self.class::Connection.new(options, &block)
+        @connection = self.class::Connection.new(config, options, &block)
       end
 
       class Connection < Ssh::Connection
+        def initialize(config, options, &block)
+          @config = config
+          super(options, &block)
+        end
+
         def upload(locals, remote)
           if @rsync_failed || !File.exists?('/usr/bin/rsync')
             logger.debug('[rsync] Rsync already failed or not installed, not trying it')
@@ -55,12 +60,14 @@ module Kitchen
           end
 
           locals = Array(locals)
+
           # We only try to sync folders for now and ignore the cache folder
           # because we don't want to --delete that.
           rsync_candidates = locals.select {|path| File.directory?(path) && File.basename(path) != 'cache' }
+
           ssh_command = "ssh #{ssh_args.join(' ')}"
-          copy_identity
-          rsync_cmd = "/usr/bin/rsync -e '#{ssh_command}' -az#{logger.level == :debug ? 'vv' : ''} --delete #{rsync_candidates.join(' ')} #{@session.options[:user]}@#{@session.host}:#{remote}"
+
+          rsync_cmd = "/usr/bin/rsync -e '#{ssh_command}' -az#{logger.level == :debug ? 'vv' : ''} --no-o --no-g --delete #{rsync_candidates.join(' ')} #{@session.options[:user]}@#{@session.host}:#{remote}"
           logger.debug("[rsync] Running rsync command: #{rsync_cmd}")
           ret = []
           time = Benchmark.realtime do
@@ -82,32 +89,14 @@ module Kitchen
           super(remaining, remote) unless remaining.empty?
         end
 
-        # Copy your SSH identity, creating a new one if needed
-        def copy_identity
-          return if @copied_identity
-          identities = Net::SSH::Authentication::Agent.connect.identities
-          raise 'No SSH identities found. Please run ssh-add.' if identities.empty?
-          key = identities.first
-          enc_key = Base64.encode64(key.to_blob).gsub("\n", '')
-          identitiy = "ssh-rsa #{enc_key} #{key.comment}"
-          @session.exec! <<-EOT
-            test -e ~/.ssh || mkdir ~/.ssh
-            test -e ~/.ssh/authorized_keys || touch ~/.ssh/authorized_keys
-            if ! grep -q "#{identitiy}" ~/.ssh/authorized_keys ; then
-              chmod go-w ~ ~/.ssh ~/.ssh/authorized_keys ; \
-              echo "#{identitiy}" >> ~/.ssh/authorized_keys
-            fi
-          EOT
-          @copied_identity = true
-        end
-
         def ssh_args
           args = %W{ -o UserKnownHostsFile=/dev/null }
           args += %W{ -o StrictHostKeyChecking=no }
-          args += %W{ -o IdentitiesOnly=yes } if @options[:keys]
+          args += %W{ -o UserKnownHostsFile=/dev/null }
+          args += %W{ -o IdentitiesOnly=yes } if @config[:ssh_key]
           args += %W{ -o LogLevel=#{@logger.debug? ? "VERBOSE" : "ERROR"} }
-          args += %W{ -o ForwardAgent=#{options[:forward_agent] ? "yes" : "no"} } if @options.key? :forward_agent
-          Array(@options[:keys]).each { |ssh_key| args += %W{ -i #{ssh_key}} }
+          args += %W{ -o ForwardAgent=#{@config[:forward_agent] ? "yes" : "no"} } if @config.key? :forward_agent
+          args += %W{ -i #{@config[:ssh_key]} } if @config[:ssh_key]
           args += %W{ -p #{@session.options[:port]}}
         end
       end
